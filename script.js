4f99ecc20a884c114e8645e7b4d64fc25f5ba8d6
Cyrborg Werewolf <alexbdelcarmen@gmail.com>
2022-04-23 18:52:32 -0600

    Add style, improve draw script, add UI

--- script.js
+++ script.js
@@ -1,23 +1,77 @@
-const mainContainer = document.querySelector('.main-container');
-let pixelNumber = 16; 
-let pixelSize = 480 / pixelNumber;
+const mainContainer = document.querySelector('.main-container'); 
 
-for (let i = 1; i <= (pixelNumber * pixelNumber); i++) {
+const deleteBtn = document.getElementById('clear');
+const slider = document.getElementById('color-slider');
+const sliderLabel = document.getElementById('slider-label');
+const colorPicker = document.getElementById('colorPicker');
+const eraserButton = document.getElementById('eraser');
+
+let pixelColor = colorPicker.value;
+let pixelNumber = slider.value;
+sliderLabel.textContent = `${pixelNumber} x ${pixelNumber}`;
+
+
+let pixelSize = 480 / pixelNumber;
+console.log(pixelSize);
+console.log(pixelNumber);
+drawBoard();
+
+function resizeBoard() {
+    clearBoard();
+    const pixels = document.querySelectorAll('.pixel');
+    pixelNumber = slider.value;
+    sliderLabel.textContent = `${pixelNumber} x ${pixelNumber}`;
+    pixelSize = 480 / pixelNumber;
+    deleteBoard();
+    drawBoard();
+}
+
+slider.addEventListener('change', resizeBoard);
+
+ function deleteBoard(){
+     const pixels = document.querySelectorAll('.pixel');
+     pixels.forEach(pixel => pixel.remove());
+
+ }
+
+function drawBoard() {
+    for (let i = 1; i <= (pixelNumber * pixelNumber); i++) {
     const pixel = document.createElement('div');
     pixel.classList.add('pixel');
     pixel.style.height = `${pixelSize}px`;
     pixel.style.width = `${pixelSize}px`;
+    pixel.style.backgroundColor = 'rgb(255,255,255)';
     pixel.id = i;
     mainContainer.appendChild(pixel);
+    }
 }
 
-const pixels = document.querySelectorAll('.pixel');
+colorPicker.addEventListener('change', () => {
+    pixelColor = colorPicker.value;
+})
+
 
 function changeColor(e) {
     const pixelToChange = document.getElementById(e.path[0].id);
-    if (pixelToChange === null) return;
-    pixelToChange.style.backgroundColor = 'rgba(255,0,0,1)';
+    if (pixelToChange === null || !pixelToChange.classList.contains('pixel')) return;
+    pixelToChange.style.backgroundColor = pixelColor;
 }
 
-pixels.forEach(pixel => addEventListener('mouseover', changeColor));
 
+
+function clearBoard() {
+    // pixels.forEach(pixel => pixel.style.backgroundColor = 'rgba(255,255,255,1)');
+    deleteBoard();
+    drawBoard();
+}
+
+deleteBtn.addEventListener('click', clearBoard);
+
+function eraser() {
+    pixelColor = 'white';
+}
+
+eraserButton.addEventListener('click', eraser); 
+
+const pixels = document.querySelectorAll('.pixel');
+pixels.forEach(pixel => addEventListener('mouseover', changeColor));
